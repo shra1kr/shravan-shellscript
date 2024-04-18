162bef326886a25aa6b0303702bc58d053eb77a5
shravan <shravankumar6262@gmail.com>
2024-04-19 00:22:26 +0530

    some-message

---
+++ 02-variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "shravan: hii ramesh this shravan how are you"
+echo "raamesh: hii shravan i am fine and how about you, what you are doing ?"
+echo "shravan: I am upgrading to DevOps and you"
+echo "ramesh:  am also looking upgrade to DevOps"
