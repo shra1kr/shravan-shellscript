a01a7beba243ce5e8d76c2fc767609aabc8dada0
shravan <shravankumar6262@gmail.com>
2024-04-19 00:38:21 +0530

    some-message

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,7 +2,7 @@
 
 Person1=shravan
 Person2=ramesh
-echo "$Person1: hii $Person1, this $Person2 how are you"
+echo "$Person1: hii $Person2, this $Person1 how are you"
 echo "$Person2: hii $Person1, i am fine and how about you, what you are doing ?"
 echo "$Person1: I am upgrading to DevOps and you"
 echo "$Person2: I am also looking upgrade to DevOps"
