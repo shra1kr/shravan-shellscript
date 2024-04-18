708daa523e2170f9dc99b00bc918307d5cf9e450
shravan <shravankumar6262@gmail.com>
2024-04-19 00:58:34 +0530

    Committed by shravan

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+echo "$Person1: hii $Person2, this $Person1 how are you"
+echo "$Person2: hii $Person1, i am fine and how about you, what you are doing ?"
+echo "$Person1: I am upgrading to DevOps and you"
+echo "$Person2: I am also looking upgrade to DevOps"
