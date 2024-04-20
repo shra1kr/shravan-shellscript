a8d8ee1b3b29996a60a7b4cd8d6ce3a3658605d2
shravan <shravankumar6262@gmail.com>
2024-04-20 10:42:05 +0530

    Committed by shravan

--- 11-condition.sh
+++ 11-condition.sh
@@ -3,7 +3,7 @@
 NUMBER=$1
 if [ $NUMBER -gt 10 ]
 then
-    echo "Given is $NUMBER greater than 10"
+    echo "Given is number $NUMBER greater than 10"
 else
-    echo  "Given is $NUMBER less than 10"
+    echo  "Given is number $NUMBER less than 10"
 fi
