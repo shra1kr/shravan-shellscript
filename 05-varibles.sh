c015aaee3e985c0210ce85a1b888996d8d9d0989
shravan <shravankumar6262@gmail.com>
2024-04-20 08:16:02 +0530

    Committed by shravan

---
+++ 05-varibles.sh
@@ -0,0 +1,7 @@
+#!/bin/bassh
+
+echo "plaese enter username:"
+read USERNAME #here USERNAME is varible
+echo "please enter password:" 
+read PASSWORD
+echo "username is: $USERNAME, password is: $PASSWORD"
