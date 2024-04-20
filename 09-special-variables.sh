4ad566e070a5860a304f9a810666b74319b7daa9
shravan <shravankumar6262@gmail.com>
2024-04-20 10:09:29 +0530

    Committed by shravan

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -3,3 +3,10 @@
 echo "All variables: $@"
 echo "Number of variables passed: $#"
 echo "Script Name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Hostname: $HOSTNAME"
+echo "Process ID of current shell-script: $$"
+sleep 60
+echo "process ID of last baground command $!"
